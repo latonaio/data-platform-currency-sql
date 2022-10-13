0fc6010bcb5095469fc0531709f28aca203e4a69
Yurino Oga <yurino.o@latona.io>
2022-10-13 16:53:20 +0900

    release

--- data-platform-currency-sql-currency-text-data.sql
+++ data-platform-currency-sql-currency-text-data.sql
@@ -3,6 +3,7 @@ CREATE TABLE `data_platform_currency_currency_text_data`
     `Currency`                     varchar(5) NOT NULL,
     `Language`                     varchar(2) NOT NULL,
     `CurrencyName`                 varchar(100) DEFAULT NULL,
+    `CurrencyLongName`             varchar(200) DEFAULT NULL,
     PRIMARY KEY (`Currency`, `Language`),
     CONSTRAINT `DPFMCurrencyCurrencyTextData_fk` FOREIGN KEY (`Currency`) REFERENCES `data_platform_currency_currency_data` (`Currency`)
 ) ENGINE = InnoDB
