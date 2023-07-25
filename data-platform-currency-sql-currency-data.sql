ecf812a78f096b6b17f2a9201da348c618758528
maimeimow <134258119+maimeimow@users.noreply.github.com>
2023-07-25 12:32:09 +0900

    release

--- data-platform-currency-sql-currency-data.sql
+++ data-platform-currency-sql-currency-data.sql
@@ -1,6 +1,11 @@
 CREATE TABLE `data_platform_currency_currency_data`
 (
-    `Currency`                  varchar(3) NOT NULL,
+    `Currency`               varchar(3) NOT NULL,
+    `CreationDate`           date NOT NULL,
+    `LastChangeDate`         date NOT NULL,
+    `IsMarkedForDeletion`    tinyint(1) DEFAULT NULL,
+    
     PRIMARY KEY (`Currency`)
+
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
--- data-platform-currency-sql-currency-text-data.sql
+++ data-platform-currency-sql-currency-text-data.sql
@@ -2,9 +2,16 @@ CREATE TABLE `data_platform_currency_currency_text_data`
 (
     `Currency`                     varchar(5) NOT NULL,
     `Language`                     varchar(2) NOT NULL,
-    `CurrencyName`                 varchar(100) DEFAULT NULL,
+    `CurrencyName`                 varchar(100) NOT NULL,
     `CurrencyLongName`             varchar(200) DEFAULT NULL,
+    `CreationDate`                 date NOT NULL,
+    `LastChangeDate`               date NOT NULL,
+    `IsMarkedForDeletion`          tinyint(1) DEFAULT NULL,
+
     PRIMARY KEY (`Currency`, `Language`),
-    CONSTRAINT `DPFMCurrencyCurrencyTextData_fk` FOREIGN KEY (`Currency`) REFERENCES `data_platform_currency_currency_data` (`Currency`)
+
+    CONSTRAINT `DPFMCurrencyTextData_fk` FOREIGN KEY (`Currency`) REFERENCES `data_platform_currency_currency_data` (`Currency`),
+    CONSTRAINT `DPFMCurrencyTextDataLanguage_fk` FOREIGN KEY (`Language`) REFERENCES `data_platform_language_language_data` (`Language`)
+  
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
